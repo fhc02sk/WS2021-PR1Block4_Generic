f2d6599ee10f22427347cad866775692b6f29a7e
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2020-12-11 19:14:34 +0100

    Profil+DemoCode

---
+++ src/socialmedia/DemoSocialMediaApp.java
@@ -0,0 +1,23 @@
+package socialmedia;
+
+public class DemoSocialMediaApp {
+    // psvm
+    public static void main(String[] args) {
+
+        Profil max = new Profil("Max@Muster.at", "Musterstraße 1", 39, 3_500);
+        max.printProfil();
+
+        Profil susi = new Profil("susi@gmail.com", "Gleisdorferstraße 12", 38, 3_700);
+        susi.printProfil();
+
+        Profil john = new Profil("john@test.com", "New York 1", 40, 3_900);
+        john.printProfil();
+
+
+        max.setBestFriend(susi);
+        max.printProfil();
+
+        susi.setBestFriend(max);
+        susi.printProfil();
+    }
+}
--- src/socialmedia/Profil.java
+++ src/socialmedia/Profil.java
@@ -1,5 +1,44 @@
 package socialmedia;
 
 public class Profil {
+    private String username;
+    private String address;
+    private int age;
+    private double salary;
+    private Profil bestFriend;
 
+    public Profil(String username, String address, int age, double salary) {
+        this.username = username;
+        this.address = address;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public void printProfil() {
+        System.out.println("Username: " + username);
+        System.out.println("Address: " + address);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+
+        if (bestFriend != null) {
+            System.out.println("Username of BestFriend: " + bestFriend.username);
+        }
+        System.out.println("*********************************************");
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void setBestFriend(Profil bestFriend) {
+        this.bestFriend = bestFriend;
+    }
 }
